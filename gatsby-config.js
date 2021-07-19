e69f2c140545e4616380e1885cd94ba24624e6d5
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-19 09:58:55 -0700

    add gatsby-plugin-google-gtag - remove gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,36 +150,15 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: '',
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: 'G-QFYXGZTN6F',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: '0',
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'spencerlepine.com',
-        // defaults to false
-        enableWebVitalsTracking: true,
+        trackingIds: [
+          'G-QFYXGZTN6F', // Google Analytics / GA
+        ],
+        pluginConfig: {
+          head: true,
+          anonymize_ip: true,
+        },
       },
     },
   ],
