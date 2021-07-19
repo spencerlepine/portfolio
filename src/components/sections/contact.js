55250435615519be5a48df31061d56dc2779b599
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-18 17:04:41 -0700

    style contact section

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -5,12 +5,14 @@ const Contact = () => (
   <section id="contact" className="landing-section">
     <h2 className="section-title">Get In Touch</h2>
 
-    <p>
+    <p className="text-xl my-5 mx-auto w-min whitespace-nowrap">
       Any questions? Want to work together? Let's connect!
     </p>
 
-    <a href={`mailto:${email}`} className="m-5 px-7 py-3 border-solid border-4 border-navy-light text-navy-dark">
-      Contact Me
+    <a href={`mailto:${email}`}>
+      <p className="w-min whitespace-nowrap mx-auto my-5 px-7 py-3 border-solid border-4 border-navy-light text-navy-dark">
+        Contact Me
+      </p>
     </a>
   </section>
 );
