f6d886377f7394396abd01f7319c5b47245d67e0
Spencer Lepine <spencerlepine26@gmail.com>
2022-08-31 15:04:56 -0700

    fix capitalize import path

--- src/styles/landingSection.js
+++ src/styles/landingSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SkewWrapper from '@styles/SkewWrapper';
+import SkewWrapper from '@styles/skewWrapper';
 
 const LandingSection = ({ children, id, isSkewSection }) => {
   if (isSkewSection) {
